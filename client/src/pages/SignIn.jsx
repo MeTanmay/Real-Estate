6a490e04863bc60e1db577cab6f681dd219c57be
Tanmay Borse <tanmayborse@Tanmays-MacBook-Air.local>
2024-03-15 00:41:31 +0530

    added redux persist

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,6 +1,6 @@
 import React, {useState} from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import { UseDispatch, useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import {signInStart, signInSuccess, signInFailure} from '../redux/user/userSlice.js'
 
 export default function SignIn() {
@@ -8,7 +8,7 @@ export default function SignIn() {
   const [formData, setFormData] = useState({});
   const {loading, error}=useSelector((state)=>state.user);
   const navigate = useNavigate();
-  const dispatch=UseDispatch();
+  const dispatch=useDispatch();
 
   const handleChange=(e)=>{
     setFormData({
