a6274e44fa3258954fdff272e2981d74af33acf7
Tanmay Borse <tanmayborse@Tanmays-MacBook-Air.local>
2024-04-12 19:30:14 +0530

    Done with update user from profile functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -3,20 +3,20 @@ import { useSelector } from 'react-redux'
 import { useRef, useState, useEffect } from 'react';
 import {getDownloadURL, getStorage, ref, uploadBytesResumable,} from 'firebase/storage';
 import { app } from '../firebase';
+import {updateUserFailure, updateUserStart, updateUserSuccess} from '../redux/user/userSlice'
+import { useDispatch } from 'react-redux';
 
 
 export default function Profile() {
 
-  // const { currentUser } = useSelector((state) => state.user);
-  // const fileRef=useRef(null);
-  // const [file, setFile] = useState(undefined);
-  // console.log(file);
-  const {currentUser} = useSelector((state) => state.user);
+  const {currentUser, loading, error} = useSelector((state) => state.user);
   const fileRef = useRef(null);
   const [file, setFile] = useState(undefined);
   const [filePerc, setFilePerc] = useState(0);
   const [fileUploadError, setFileUploadError] = useState(false);
   const [formData, setFormData] = useState({});
+  const [updateSuccess, setUpdateSuccess] = useState(false);
+  const dispatch = useDispatch();
 
 //   // console.log(formData);
 //   // console.log(filePerc);
@@ -64,13 +64,40 @@ export default function Profile() {
         }
       );
     };
+  
+  const handleChange=(e)=>{
+    setFormData({ ...formData, [e.target.id]: e.target.value })
+  };
 
+  const handleSubmit=async(e)=>{
+    e.preventDefault();
+    try {
+      dispatch(updateUserStart());
+      const res = await fetch(`/api/user/update/${currentUser._id}`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        dispatch(updateUserFailure(data.message));
+        return;
+      }
 
+      dispatch(updateUserSuccess(data));
+      setUpdateSuccess(true);
+    } catch (error) {
+      dispatch(updateUserFailure(error.message));
+    }
+
+  }
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className="text-3xl font-semibold text-center my-10" >Profile</h1>
 
-      <form className='flex flex-col gap-4'>
+      <form onSubmit={handleSubmit}className='flex flex-col gap-4'>
 
         <input
           onChange={(e) => setFile(e.target.files[0])}
@@ -103,10 +130,12 @@ export default function Profile() {
           </p>
 
         
-        <input type="text" placeholder='username' className='border p-3 rounded-lg' id= 'username'/>
-        <input type="text" placeholder='email' className='border p-3 rounded-lg' id= 'email'/>
-        <input type="text" placeholder='password' className='border p-3 rounded-lg' id= 'password'/>
-        <button className='border p-3 rounded-lg bg-slate-700 text-white uppercase hover:opacity-95 disabled: opacity-80'>Update</button>
+        <input type="text" placeholder='username' defaultValue= {currentUser.username} className='border p-3 rounded-lg' id= 'username' onChange={handleChange}/>
+        <input type="text" placeholder='email' defaultValue= {currentUser.email} className='border p-3 rounded-lg' id= 'email' onChange={handleChange}/>
+        <input type="password" placeholder='password' className='border p-3 rounded-lg' id= 'password' onChange={handleChange}/>
+        <button disabled={loading} className='border p-3 rounded-lg bg-slate-700 text-white uppercase hover:opacity-95 disabled: opacity-80'>
+          {loading ? "Loading..." :"Update"}
+        </button>
         
 
 
@@ -115,6 +144,8 @@ export default function Profile() {
         <span className='text-red-700 cursor-pointer'>Delete Account</span>
         <span className='text-red-700 cursor-pointer'>Sign Out</span>
       </div>
+      <p className="text-red-700 mt-5">{error ? error :''}</p>
+      <p className="text-green-700 mt-5">{updateSuccess ? "User is updated succesfully" :''}</p>
     </div>
   )
 }
