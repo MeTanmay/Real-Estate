c2d23f71d4f1b0c75968cd4f824584501b875f9b
Tanmay Borse <tanmayborse28@gmail.com>
2024-06-14 18:53:42 +0530

    search page UI..

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,6 +10,7 @@ import PrivateRoute from './components/PrivateRoute'
 import CreateListing from './pages/CreateListing'
 import UpdateListing from './pages/UpdateListing'
 import Listing from './pages/Listing'
+import Search from './pages/Search'
 
 
 const App = () => {
@@ -22,6 +23,7 @@ const App = () => {
         <Route path='/sign-up' element={<SignOut />} />
         <Route path='/about' element={<About />} />
         <Route path='/listing/:listingId' element={<Listing />} />
+        <Route path='/search' element={<Search />} />
         <Route element={<PrivateRoute />}>
           <Route path='/profile' element={<Profile />} />
           <Route path='/create-listing' element={<CreateListing />} />
